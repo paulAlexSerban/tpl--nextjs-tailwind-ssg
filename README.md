2aed6004d5408798b8024240600f692e36bc8199
paulAlexSerban <paul.alex.serban@gmail.com>
2022-09-22 21:33:04 +0300

    feat(init): add initial configuration

--- README.md
+++ README.md
@@ -1,7 +1,2 @@
-# tpl--clean-boilerplate
+# tpl--nextjs-tailwind-ssg
 
-## How to use:
-1. go to `./config.end`
-2. change the namings as desired
-3. RUN `bash setupProject.bash` to rename configuration files
-4. DELETE `setupProject.bash` as the file is needed ony to setup a new project
--- release.config.js
+++ release.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   branches: "release",
-  repositoryUrl: "https://github.com/paulAlexSerban/tpl--clean-boilerplate",
+  repositoryUrl: "https://github.com/paulAlexSerban/tpl--nextjs-tailwind-ssg",
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
