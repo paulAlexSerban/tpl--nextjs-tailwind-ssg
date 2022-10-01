54a4fd75cb7a52fda072fc14cc8ccba7c0d400eb
paulAlexSerban <paul.alex.serban@gmail.com>
2022-10-01 15:16:28 +0300

    feat(category): added category sidebar

--- backend/devspace/pages/blog/category/[category_name].js
+++ backend/devspace/pages/blog/category/[category_name].js
@@ -2,64 +2,65 @@ import fs from "fs";
 import path from "path";
 import Layout from "@/components/Layout";
 import Post from "@/components/Post";
-import { sortByDate } from "@/utils/index";
+import CategoryList from "@/components/CategoryList";
 import matter from "gray-matter";
-import Link from "next/link";
-export default function CategoryBlogPage({ posts, categoryName }) {
+import { getPosts } from "@/library/posts";
+export default function CategoryBlogPage({ posts, categoryName, categories }) {
   return (
     <Layout>
-      <h1 className="text-5xl border-b-4 p-5 font-bold">Posts in {categoryName}</h1>
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
-        {posts.map((post, index) => (
-          <Post key={index} post={post} />
-        ))}
+      <div className="flex justify-between">
+        <div className="w-3/4 mr-10">
+        <h1 className="text-5xl border-b-4 p-5 font-bold">Posts in {categoryName}</h1>
+          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+            {posts.map((post, index) => (
+              <Post key={index} post={post} />
+            ))}
+          </div>
+        </div>
+        <div className="w-1/4">
+          <CategoryList categories={categories}/>
+        </div>
       </div>
     </Layout>
   );
 }
 
 export async function getStaticPaths() {
-  const files = fs.readdirSync(path.join('posts'));
-  const categories = files.map(filename => {
-    const markdownWithMeta = fs.readFileSync(path.join('posts', filename), 'utf-8');
-    const {data: frontmatter} = matter(markdownWithMeta);
+  const files = fs.readdirSync(path.join("posts"));
+  const categories = files.map((filename) => {
+    const markdownWithMeta = fs.readFileSync(path.join("posts", filename), "utf-8");
+    const { data: frontmatter } = matter(markdownWithMeta);
 
     return frontmatter.category.toLowerCase();
-  })
+  });
 
-  const paths = categories.map(category => ({
-    params:{
-      category_name: category
-    }
-  }))
+  const paths = categories.map((category) => ({
+    params: {
+      category_name: category,
+    },
+  }));
 
   return {
     paths,
-    fallback: false
-  }
+    fallback: false,
+  };
 }
 
-export async function getStaticProps({params: {category_name}}) {
-  const files = fs.readdirSync(path.join("posts")); //read and get th posts
+export async function getStaticProps({ params: { category_name } }) {
+  const posts = getPosts();
 
-  const posts = files.map((filename) => {
-    const slug = filename.replace(".md", "");
-    const markdownWithMeta = fs.readFileSync(path.join("posts", filename), "utf-8");
-    const { data: frontmatter } = matter(markdownWithMeta);
-    return {
-      slug,
-      frontmatter,
-    };
-  });
+  // getCategoriesForSidebar
+  const categories = posts.map((post) => post.frontmatter.category);
+  const uniqueCategories = [...new Set(categories)];
 
   // filter posts by category
-  const categoryPosts = posts.filter(post => post.frontmatter.category.toLowerCase() === category_name)
+  const categoryPosts = posts.filter((post) => post.frontmatter.category.toLowerCase() === category_name);
 
   return {
     props: {
-      posts: categoryPosts.sort(sortByDate),
+      posts: categoryPosts,
       categoryName: category_name,
-
+      categories: uniqueCategories
     },
   };
 }
