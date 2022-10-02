70ef871b6b1b98ad65d0e8912416088483021edf
paulAlexSerban <paul.alex.serban@gmail.com>
2022-10-02 18:14:07 +0300

    fix(ci): aws amplify issues

--- backend/devspace/pages/api/search.js
+++ backend/devspace/pages/api/search.js
@@ -9,7 +9,7 @@ export default function handler(req, res) {
 
   if (process.env.NODE_ENV === "production") {
     // Fetch from ./cache
-    posts = requrie('../../cache/data.js').posts
+    posts = require('../../cache/data.js').posts
   } else {
     const files = fs.readdirSync(path.join("posts"));
     posts = files.map((filename) => {
